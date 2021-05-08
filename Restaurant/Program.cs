7cbdfc679175ef13ad5b0c161edfe1ae7cbea5cd
Eric Fritts <eric.fritts@live.com>
2021-05-07 20:51:10 -0500

    Initial commit

---
+++ Restaurant/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Restaurant
+{
+    public class RestaurantPick
+    {
+        #region You_should_not_modify_this_region
+
+
+        private class Restaurant
+        {
+            public int RestaurantId { get; set; }
+            public Dictionary<string, decimal> Menu { get; set; }
+        }
+
+        private readonly List<Restaurant> _restaurants = new List<Restaurant>();
+
+        /// <summary>
+        /// Reads the file specified at the path and populates the restaurants list
+        /// </summary>
+        /// <param name="filePath">Path to the comma separated restuarant menu data</param>
+        public void ReadRestaurantData(string filePath)
+        {
+            try
+            {
+                var records = File.ReadLines(filePath);
+
+                foreach (var record in records)
+                {
+                    var data = record.Split(',');
+                    var restaurantId = int.Parse(data[0].Trim());
+                    var restaurant = _restaurants.Find(r => r.RestaurantId == restaurantId);
+
+                    if (restaurant == null)
+                    {
+                        restaurant = new Restaurant { Menu = new Dictionary<string, decimal>() };
+                        _restaurants.Add(restaurant);
+                    }
+
+                    restaurant.RestaurantId = restaurantId;
+                    restaurant.Menu.Add(data.Skip(2).Select(s => s.Trim()).Aggregate((a, b) => a.Trim() + "," + b.Trim()), decimal.Parse(data[1].Trim()));
+                }
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var restaurantPicker = new RestaurantPick();
+            
+            restaurantPicker.ReadRestaurantData(
+                Path.GetFullPath(
+                    Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory, @"../../../../restaurant_data.csv")
+                    )
+                );
+
+            // Item is found in restaurant 2 at price 6.50
+            var bestRestaurant = restaurantPicker.PickBestRestaurant("gac");
+
+            Console.WriteLine(bestRestaurant.Item1 + ", " + bestRestaurant.Item2);
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        #endregion
+
+
+
+        #region You_can_modify_this_region
+        /// <summary>
+        /// Takes in items you would like to eat and returns the best restaurant that serves them.
+        /// </summary>
+        /// <param name="items">Items you would like to eat (seperated by ',')</param>
+        /// <returns>Restaurant Id and price tuple</returns>
+        public Tuple<int, decimal> PickBestRestaurant(string items)
+        {   /*
+             *
+             * Put your solution here
+             *
+             *
+             */
+
+            string[] rs = items.Split(',');
+
+            List<int> restList = new List<int>();
+
+            //  1. Search through _restaurants and make list of places which have all items requested
+
+            
+            
+            //  2. Search through list and find cheapest combo of all items per restaurant
+
+            //  3. Return cheapest
+
+
+           
+
+           
+           
+
+           
+            return new Tuple<int, decimal>(0, 0);
+        }
+
+        #endregion
+    }
+}
